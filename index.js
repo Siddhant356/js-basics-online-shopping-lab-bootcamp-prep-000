ddcc74ec68d4b2d6154d6d08e9dd5117dee27d16
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-30 16:48:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber===undefined)
-  console.log(`Sorry, we don't have a credit card on file for you.`)
+  return `Sorry, we don't have a credit card on file for you.`
   else{
     var tot=total()
     cart.splice(0,cart.length)
